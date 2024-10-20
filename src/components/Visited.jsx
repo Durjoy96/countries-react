8430b0e84752d2f6a983ac2cf0df8dec858556a1
Durjoy96 <mddurjoy853@gmail.com>
2024-10-20 20:37:58 +0600

    Nothing to show msg added

--- src/components/Visited.jsx
+++ src/components/Visited.jsx
@@ -1,13 +1,15 @@
 import Country from "./Country";
 
 const Visited = ({ data, isDisplay }) => {
-  console.log(data);
   return (
     <div className={` mt-12 ${isDisplay ? "inline-block" : "hidden"}`}>
       <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-4">
         {data.map((item) => (
           <Country key={item.cca2} data={item} showBtn={false}></Country>
         ))}
+        <p className="text-red-500 text-lg font-normal absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {data.length ? "" : "Nothing to Show!"}
+        </p>
       </div>
     </div>
   );
